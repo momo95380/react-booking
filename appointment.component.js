e4be2470b296ef66a4a21aedc02d3810fac40103
Mohammed <m.abdelaziz1994@gmail.com>
2021-01-14 12:28:18 +0100

    remove import

--- appointment.component.js
+++ appointment.component.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import 'react-time-picker/dist/TimePicker.css';
-
 var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
